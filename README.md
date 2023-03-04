04258b9dd61ddb9af0ee6d755c842934973b7c2c
Ladbaby <1390265970@qq.com>
2023-03-04 23:31:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,44 @@
+﻿# Shell Script for [pdf.tocgen](https://github.com/Krasjet/pdf.tocgen)
+
+[pdf.tocgen](https://github.com/Krasjet/pdf.tocgen) is "A CLI toolset to generate table of contents for PDF files (semi?) automatically"
+
+However, the usage of its commands seems to be not so convinient. To further speedup the process and make its usage more intuitive, this shell script is used.
+
+## Highlights
+
+- Intuitive interface
+- No need to copy the source pdf file to script's path (**Even in WSL**)
+- No need to copy the output file back to source file's path
+
+
+## Compatibility
+
+**Optimized for Windows Subsystem for Linux**. Also works in pure Linux (sounds strange right? see the usage section).
+
+## Installation
+
+Please refer to [pdf.tocgen](https://github.com/Krasjet/pdf.tocgen)'s installation instruction
+
+## Usage
+
+After changing to the script's path:
+```shell
+sh TOC_PDF.sh
+```
+
+e.g.
+
+```
+-----TOC generator for PDF-----
+Enter the file name:"C:\Users\UserName\Apps\Zotero\database\storage\3XCKGZAK\Yang et al_2022_Diffusion Models.pdf"
+Enter the maximum depth of TOC (default: 2):1
+Enter the page number of level 1's title  (default: 1):3
+Enter the title name of level 1 (default: "Introduction"):INTRODUCTION
+-----PDF file with TOC has been generated!-----
+```
+
+which will output the result file to the source file's folder, with the name suffix `_output.pdf`
+
+> 💡There's **no need to copy the source file to script's folder**! When specifying the file name (absolute path), **Windows file path is supported**! which will automatically be convert it to `/mnt/...`. Linux file path (e.g., `../filename.pdf`) is also supported
+
+
---
+++ TOC_PDF.sh
@@ -0,0 +1,54 @@
+clear
+
+echo "-----\033[42mTOC generator for PDF\033[0m-----"
+
+# prompt for file name
+echo -n "Enter the \033[31mfile name\033[0m:"
+read -r abs_path_windows
+if [ -n "$abs_path_windows" ]; then
+    abs_path_linux=$(printf "%s" "$abs_path_windows" | sed 's/\\/\//g; s/\([A-Za-z]\):/\/mnt\/\L\1/')
+    abs_path_linux=$(printf "%s" "$abs_path_linux" | sed "s/\"//g")
+    
+    pdf_file_name=$(basename "$abs_path_linux")
+    cp "$abs_path_linux" "."
+    destination_path="${abs_path_linux%.pdf}_out.pdf"
+    output_file_name="${pdf_file_name%.pdf}_out.pdf"
+fi
+
+# prompt for TOC max depth
+echo -n "Enter the \033[31mmaximum depth\033[0m of TOC (default: 2):"
+read max_depth
+max_depth=${max_depth:-2}
+
+# prompt for recipe for each level
+for i in $(seq 1 $max_depth); do
+    # page number of the title
+    echo -n "Enter the \033[31mpage number\033[0m of \033[32mlevel $i\033[0m's title  (default: 1):"
+    read page_number
+    if [ -n "$page_number" ]; then
+        page_number=$(expr "$page_number" + 0)
+    fi
+    page_number=${page_number:-1}
+
+    # title name
+    echo -n "Enter the \033[31mtitle name\033[0m of \033[32mlevel $i\033[0m (default: \"Introduction\"):"
+    read title
+    title=${title:-"Introduction"}
+
+    # write to recipe.toml
+    pdfxmeta -p "${page_number}" -a 1 "${pdf_file_name}" "${title}" >> recipe.toml
+done
+
+# generate toc file using recipe
+pdftocgen "${pdf_file_name}" < recipe.toml > toc
+
+# edit the generated toc
+vim toc
+
+# insert recipe into pdf file
+pdftocio "${pdf_file_name}" < toc
+
+mv "$pdf_file_name" "$destination_path"
+rm recipe.toml toc "$output_file_name"
+
+echo "-----\033[44mPDF file with TOC has been generated!\033[0m-----"
